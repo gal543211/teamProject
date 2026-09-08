b71f09c82d7c332daeac90f6af18301a1196d82e
jbt <karny4848@gmail.com>
2026-09-08 14:14:39 +0300

    adding stuff to main

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pygame
 import consts
 import screen
-
+import game_field
 
 def main():
     pygame.init()
     running = True
-
+    field = game_field.create_field()
 
     while running:
         code = handle_events()
@@ -14,6 +14,7 @@ def main():
         if code == consts.EXIT:
             running = False
 
+        screen.print_screen(field)
 
 
 def handle_events():
